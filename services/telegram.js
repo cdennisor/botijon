ac3c0a5833435358ad254f27e9c1b7fae8eb5ce1
cdennisor <1479649@senati.pe>
2025-02-15 13:42:39 -0500

    c6

--- services/telegram.js
+++ services/telegram.js
@@ -3,7 +3,7 @@ import { telegram_api, telegram_token } from "../utils/config";
 export async function sendToTelegram(chatID, message) {
     return fetch(`${telegram_api}/sendMessage`,{
         method:"POST",
-        headers:{"Content-type":"application/json"},
+        headers:{"Content-Type":"application/json"},
         body:JSON.stringify({chat_id:chatID,text:message}),
     });
 }
